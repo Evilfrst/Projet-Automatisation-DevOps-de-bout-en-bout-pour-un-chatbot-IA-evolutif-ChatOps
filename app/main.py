52d834fc9ed2884cdb771a18c40048a1ac60de8f
EvradDsoh <wqbw57ccsb@privaterelay.appleid.com>
2026-04-25 09:09:42 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,16 +2,29 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from chatbot import get_response
 
+from prometheus_client import Counter, generate_latest
+from fastapi.responses import Response
+
 app = FastAPI()
+
+# 📊 Metrics
+REQUEST_COUNT = Counter("app_requests_total", "Total requests")
 
 class Message(BaseModel):
     text: str
 
 @app.get("/")
 def read_root():
+    REQUEST_COUNT.inc()
     return {"status": "Chatbot API running"}
 
 @app.post("/chat")
 def chat(message: Message):
+    REQUEST_COUNT.inc()
     response = get_response(message.text)
     return {"response": response}
+
+# 🔥 Endpoint Prometheus
+@app.get("/metrics")
+def metrics():
+    return Response(generate_latest(), media_type="text/plain")
