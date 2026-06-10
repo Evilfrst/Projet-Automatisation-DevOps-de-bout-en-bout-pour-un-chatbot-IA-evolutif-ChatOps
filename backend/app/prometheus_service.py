5e8b459e20118d954ec13de41f28019f70133321
EvradDsoh <wqbw57ccsb@privaterelay.appleid.com>
2026-06-10 17:39:27 +0100

    DevOps final

--- backend/app/prometheus_service.py
+++ backend/app/prometheus_service.py
@@ -24,3 +24,23 @@ def query_prometheus(query: str):
 def cluster_health():
 
     return query_prometheus("up")
+
+def cpu_usage():
+
+    return query_prometheus(
+        '100 - (avg(irate(node_cpu_seconds_total{mode="idle"}[5m])) * 100)'
+    )
+
+
+def memory_usage():
+
+    return query_prometheus(
+        '(1 - (node_memory_MemAvailable_bytes / node_memory_MemTotal_bytes))*100'
+    )
+
+
+def pod_count():
+
+    return query_prometheus(
+        'count(kube_pod_info)'
+    )
