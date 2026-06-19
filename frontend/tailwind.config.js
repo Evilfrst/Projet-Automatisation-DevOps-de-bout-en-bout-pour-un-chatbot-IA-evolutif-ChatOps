b71d856e24d5c5d3653979c9afebb8e4c558e836
EvradDsoh <wqbw57ccsb@privaterelay.appleid.com>
2026-06-19 22:20:31 +0100

    DevOps final

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,14 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-const config = {
-  content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+module.exports = {
+content: [
+"./app/**/*.{js,ts,jsx,tsx}",
+"./pages/**/*.{js,ts,jsx,tsx}",
+"./components/**/*.{js,ts,jsx,tsx}",
+],
+theme: {
+extend: {},
+},
+plugins: [],
 }
-
-export default config
